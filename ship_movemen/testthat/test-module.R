eb53b40080869d433296a995bf10a83513e6c2ee
casuallygaussian <52837301+casuallygaussian@users.noreply.github.com>
2021-10-06 13:14:20 +0200

    Delete ship_movemen/testthat directory

--- ship_movemen/testthat/test-module.R
+++
@@ -1,9 +0,0 @@
-# See ?testServer for more information
-testServer(DropDownMod, {
-  # Set initial value of a button
-  session$setInputs(ship_type = 'Cargo')
-
-  # Check the value of the reactiveVal `count()`
-  expect_equal(ship_namest()[1:3], c("AALDERDIJK" , "ADAMAS" ,"ADASTRA"))
-  
-})
--- ship_movemen/testthat/test-server.R
+++
@@ -1,6 +0,0 @@
-testServer(expr = {
-
-  session$setInputs(ship_type = 'Cargo')
-  expect_equal(output$distance_info, "Traveled 1104.79 meters at 33.71 Km/h")
-
-})
